57cf1e62e868c153c1e6975146400849f32bd162
Inluminous <lenmisch@gmail.com>
2021-05-06 23:00:43 +0200

    Setup local module installation

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='xaibenchmark',
+        version='0.1',
+        description='Benchmark for local model-agnostic explainers',
+        url='',
+        packages=['xaibenchmark'],
+        zip_safe=False)
