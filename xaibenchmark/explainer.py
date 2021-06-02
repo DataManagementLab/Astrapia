ad4bba329169714d3ee50ab43b304a0da52865ac
Inluminous <lenmisch@gmail.com>
2021-06-02 17:17:27 +0200

    Clean and document explainer.py

--- xaibenchmark/explainer.py
+++ xaibenchmark/explainer.py
@@ -1,7 +1,10 @@
 
 
 def metric(fn):
-    # mark the method as something that can be calculated as a metric
+    """Decorator for tagging metrics.
+    
+    Metrics can be used to compare different explainers.
+    """
     def wrapper(*args):
         result = fn(*args)
         if result is None:
@@ -11,6 +14,10 @@ def metric(fn):
     return wrapper
 
 def utility(fn):
+    """Decorator for tagging utility functions.
+
+    Utility functions can be used to infer different metrics automatically.   
+    """
     # mark the method as something that can be used to infer metrics
     def wrapper(*args):
         result = fn(*args)
@@ -22,59 +29,55 @@ def utility(fn):
 
 
 class Explainer:
+    """Base class for wrapping and comparing Explainers
     
+    Add metrics using the @metric decorator.
+    Add utility functions using the @utility decorator.
+
+    When using the following predefined metrics and utilities, the library can infer other metrics by calling 
+        your_explainer.infer_metrics()
+
+    Metrics:
+    - coverage(self)
+    - 
+
+    Utilities:
+    - distance(self, x, y)
+    - get_neighborhood_instances(self)
+    -
+    """
     
     def __init__(self):
         raise NotImplementedError
         
     def metrics(self):
-                
-        def checkImplementedMetric(f):
-            return True or f() != None
-        
-        all_metrics_strings = [x for x in dir(self) if getattr(getattr(self, x), 'tag', None) == 'metric']
-        all_metrics = [getattr(self, m) for m in all_metrics_strings]
-        implemented_metrics = [metric for metric, metric_name in zip(all_metrics, all_metrics_strings) if checkImplementedMetric(metric)]
-        
-        implemented_metric_names = set([metric_name for metric, metric_name in zip(all_metrics, all_metrics_strings) if checkImplementedMetric(metric)])
-        return implemented_metric_names
+        return [x for x in dir(self) if getattr(getattr(self, x), 'tag', None) == 'metric']
     
     def infer_metrics(self):
         
-        def checkImplementedMetric(f):
-            return True or f() != None
-    
-        
-        all_mu_identifiers = [x for x in dir(self) if getattr(getattr(self, x), 'tag', None) in ['metric', 'utility']]
-        all_mu_references = [getattr(self, m) for m in all_mu_identifiers]
-        implemented_mu_references = {mu for mu in all_mu_references if checkImplementedMetric(mu)}
-        implemented_mu_names = set([metric_name for metric, metric_name in zip(all_mu_references, all_mu_identifiers) if checkImplementedMetric(metric)])
-        
+        # This should be set somewhere else
         transfer_graph = [
             ({'coverage'}, 'inverse_coverage', metric(lambda : 1 / self.coverage())),
             ({'distance', 'get_explained_instance', 'get_neighborhood_instances'}, 'furthest_distance', metric(lambda : max(0, 0, *[self.distance(self.get_explained_instance(), i) for i in self.get_neighborhood_instances()]))),
         ]
+
+        mu_identifiers = {x for x in dir(self) if getattr(getattr(self, x), 'tag', None) in ['metric', 'utility']}
         
         old_mu_identifiers = {}
-        new_mu_identifiers = implemented_mu_names
+        new_mu_identifiers = mu_identifiers
         while (new_mu_identifiers != old_mu_identifiers):
             for transition in transfer_graph:
                 if transition[0] <= new_mu_identifiers:
                     setattr(self, transition[1], transition[2])
                     
             old_mu_identifiers = new_mu_identifiers
-            all_mu_identifiers = [x for x in dir(self) if getattr(getattr(self, x), 'tag', None) == 'metric']
-            all_mu_references = [getattr(self, m) for m in all_mu_identifiers]
-            new_mu_identifiers = {metric_name for metric, metric_name in zip(all_mu_references, all_mu_identifiers) if checkImplementedMetric(metric)}
+            new_mu_identifiers = {x for x in dir(self) if getattr(getattr(self, x), 'tag', None) == 'metric'}
             
         print('inferred metrics:', new_mu_identifiers)
         
     def report(self):
         
-        def checkImplementedMetric(f):
-            return True or f() != None
-        
         all_mu_identifier_references = {(x, getattr(self, x)) for x in dir(self) if getattr(getattr(self, x), 'tag', None) == 'metric'}
-        implemented_mu_values = {(x, f()) for (x, f) in all_mu_identifier_references if checkImplementedMetric(f)}
+        implemented_mu_values = {(x, f()) for (x, f) in all_mu_identifier_references}
         return implemented_mu_values
                 
