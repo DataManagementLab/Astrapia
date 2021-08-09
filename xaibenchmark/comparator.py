76f99f526c7f1f9ed7f579a37c0809e4126b3439
Julie Fang <j47548081@gmail.com>
2021-08-09 19:45:49 +0200

    add visualization for comparator

--- xaibenchmark/comparator.py
+++ xaibenchmark/comparator.py
@@ -1,4 +1,7 @@
 from collections import defaultdict
+from xaibenchmark.utils import normalize
+import plotly.graph_objects as go
+import pandas as pd
 
 
 class ExplainerComparator:
@@ -71,12 +74,16 @@ class ExplainerComparator:
             self.metrics[name] = aggregated_explainer_metrics
             self.explanations[name] = aggregated_explanations
 
-    def print_metrics(self, explainer=None, index=None):
+    def print_metrics(self, explainer=None, index=None, plot=None):
         """
         Output metrics of the explanations on the console. Either averaged metrics or metrics from single explanations
         :param explainer: Optional, in case you only want metrics from one explainer
         :param index: Optional, in case you only want metrics from one explanation
+        :param plot: Optional. Visualize the metrics in bar chart or table form. Options: 'bar' or 'table'
         """
+
+        assert plot is None or plot == 'bar' or plot == 'table', 'Wrong input. Check again.'
+
         if explainer is not None:
             if index is not None:
                 output = self.metrics[explainer][index]
@@ -85,11 +92,63 @@ class ExplainerComparator:
             else:
                 output = self.averaged_metrics[explainer]
                 print("Average metric values for the explainer", explainer, ":")
+
+
             for metric, value in output.items():
                 print("\t", metric, ":", value)
+
+            pair = list(zip(output.keys(), output.values()))
+            normalized_pair = normalize(pair)
+
+            if plot == 'bar':
+                fig = go.Figure([go.Bar(x=[metric for metric, _ in normalized_pair],
+                                        y=[value for _, value in normalized_pair],
+                                        marker_color='#96a48b')])
+
+                title = '' if index else 'Average'
+                fig.update_layout(title_text=f'{title} Metric Comparison using {explainer}',
+                                  plot_bgcolor='#fffaf4')
+                fig.show()
+            elif plot == 'table':
+                fig = go.Figure(data=[go.Table(
+                    header=dict(values=['metric', explainer],
+                                line_color='#bfbfbf',
+                                fill_color='#e05e5df',
+                                align='left'),
+                    cells=dict(values=[[metric for metric, _ in normalized_pair],  # 1st column
+                                       [value for _, value in normalized_pair]],  # 2nd column
+                               line_color='#bfbfbf',
+                               fill_color='#ve05e5df',
+                               align='left'))
+                ])
+                fig.show()
 
         else:
             for name, metrics in self.averaged_metrics.items():
                 print("Average metric values for explainer", name, ":")
                 for metric, value in metrics.items():
                     print("\t", metric, ":", value)
+
+                pair = list(zip(metrics.keys(), metrics.values()))
+                normalized_pair = normalize(pair)
+
+                if plot == 'bar':
+                    fig = go.Figure([go.Bar(x=[metric for metric, _ in normalized_pair],
+                                            y=[value for _, value in normalized_pair],
+                                            marker_color='#7b8b6f')])
+                    fig.update_layout(title_text=f'Metric Comparison',
+                                      plot_bgcolor='#ececea')
+                    fig.show()
+                elif plot == 'table':
+                    fig = go.Figure(data=[go.Table(
+                        header=dict(values=['metric', explainer],
+                                    line_color='#bfbfbf',
+                                    fill_color='#ve05e5df',
+                                    align='left'),
+                        cells=dict(values=[[metric for metric, _ in normalized_pair],  # 1st column
+                                           [value for _, value in normalized_pair]],  # 2nd column
+                                   line_color='#bfbfbf',
+                                   fill_color='#ve05e5df',
+                                   align='left'))
+                    ])
+                    fig.show()
--- xaibenchmark/utils.py
+++ xaibenchmark/utils.py
@@ -12,3 +12,19 @@ def model_properties( y_test, modelpredictions, labels=[]):
         return metrics.classification_report(y_test, modelpredictions, output_dict=True)
     else:
         return metrics.classification_report(y_test, modelpredictions, labels=labels, output_dict=True)
+
+
+def normalize(lst):
+    """
+    only normalize non-relative metrics, which is out of the [0,1] range
+    """
+    new_lst = []
+    max_val = max([i for _, i in lst])
+    min_val = min([i for _, i in lst])
+    for metric, score in lst:
+        if not 0<score<1:
+            temp = (score - min_val) / (max_val - min_val)
+            new_lst.append((metric, round(temp, 4)))
+        else:
+            new_lst.append((metric, round(score, 4)))
+    return sorted(new_lst, key=lambda x: x[0])
